f8138df3d52b636748e76cb69379d767e1cb7ab2
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-06-28 22:00:50 +0300

    Generates Loans.php model based on Loans table

---
+++ models/Loans.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "Loans".
+ *
+ * @property integer $loanId
+ * @property integer $userId
+ * @property string $amount
+ * @property string $interest
+ * @property integer $duration
+ * @property string $dateApplied
+ * @property string $dateLoanEnds
+ * @property integer $campaign
+ * @property boolean $status
+ */
+class Loans extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'Loans';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['userId', 'amount', 'interest', 'duration', 'dateApplied', 'dateLoanEnds', 'campaign'], 'required'],
+            [['userId', 'duration', 'campaign'], 'integer'],
+            [['amount', 'interest'], 'number'],
+            [['dateApplied', 'dateLoanEnds'], 'safe'],
+            [['status'], 'boolean'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'loanId' => 'Loan ID',
+            'userId' => 'User ID',
+            'amount' => 'Amount',
+            'interest' => 'Interest',
+            'duration' => 'Duration',
+            'dateApplied' => 'Date Applied',
+            'dateLoanEnds' => 'Date Loan Ends',
+            'campaign' => 'Campaign',
+            'status' => 'Status',
+        ];
+    }
+}
